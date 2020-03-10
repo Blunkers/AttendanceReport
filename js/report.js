d3ecd6e4466357c8180feb191bc112d995150bc7
Blunkers <61578597+Blunkers@users.noreply.github.com>
2020-03-10 11:52:48 -0600

    Update report.js
    cors-escape

--- js/report.js
+++ js/report.js
@@ -424,7 +424,7 @@ $("#report-container").show();
 
 var wclurl = 'http://www.warcraftlogs.com/guild/attendance-table/' + guildID + '/0?page=1'
 
-$.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(wclurl) + '&callback=?', function(data){
+$.getJSON('https://cors-escape.herokuapp.com/' + encodeURIComponent(wclurl), function(data){
 	alert(data.contents);
 });
 
